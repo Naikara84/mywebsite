5d088f37969932345f843456b9909b57f6f9fc3f
Timothy Naikara <silvernathan84@gmail.com>
2024-04-12 09:02:25 +0300

    this is my first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+let header =document.querySelector('header');
+let menu =document.querySelector('#menu-icon');
+let navbar =document.querySelector('.navbar');
+
+window.addEventListener('scroll', ()=> {
+    header.classList.toggle('shadow',window.scrollY >0);
+});
+menu.onclick =() =>{
+    navbar.classList.toggle('active');
+}
+window.onscroll =() =>{
+    navbar.classList.remove('active');
+}
+
+
+var typingEffect = new Typed(".typedText",{
+    strings:["Software Developer","Designer", "Illustrator"],
+    loop:true,
+    typeSpeed:100,
+    backSpeed:80,
+    backDelay:2000,
+
+});
+
+const sr = ScrollReveal({
+    origin:"top",
+    distance:"80px",
+    duration: "2000",
+    reset: "true",
+
+});
+sr.reveal(".social-icons", {});
+sr.reveal(".featured-name", {delay: 200});
+sr.reveal(".hello", {delay: 200});
+sr.reveal(".featured-text-info", {delay: 200});
+sr.reveal(".featured-image", {delay: 200});
+sr.reveal(".heading", {delay: 200});
+sr.reveal(".sevices-content", {interval: 200});
+
+
+const srLeft = ScrollReveal({
+    origin:"left",
+    distance:"80px",
+    duration:2000,
+    reset:true
+});
+
+srLeft.reveal(".about-container", {delay: 200});
+srLeft.reveal(".contact-form", {delay: 100});
+
+const srRight = ScrollReveal({
+    origin:"right",
+    distance:"80px",
+    duration:2000,
+    reset:true
+});
+
+srRight.reveal(".portfolio-content", {delay: 200});
+srRight.reveal(".skill", {delay: 100});
